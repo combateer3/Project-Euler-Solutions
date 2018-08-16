d4f822a206b343e3f401959c6ecf9ae0395b6635
Scott <scottcorbat@yahoo.com>
2018-08-16 10:48:04 -0400

    problem 4 solved

--- src/level1/Problem4.java
+++ src/level1/Problem4.java
@@ -2,8 +2,39 @@ package level1;
 
 public class Problem4 {
 
-    public static void solve() {
+    /* A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
 
+        Find the largest palindrome made from the product of two 3-digit numbers. */
+
+    public static void solve() {
+        int palindrome = 0;
+        int product;
+
+        for (int i = 100; i < 1000; i++) {
+            for (int j = 100; j < 1000; j++) {
+                product = i * j;
+                if (isPalindrome(product) && product > palindrome) {
+                    palindrome = product;
+                }
+            }
+        }
+
+        System.out.println(palindrome);
+    }
+
+    private static boolean isPalindrome(int num) {
+        String[] digits = String.valueOf(num).split("");
+        String reverse = "";
+
+        for (int i = 0; i < digits.length; i++) {
+            reverse += digits[digits.length - 1 - i];
+        }
+
+        if (Integer.parseInt(reverse) == num) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,11 @@
 package main;
 
+import level1.Problem4;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        Problem4.solve();
     }
 
 }
